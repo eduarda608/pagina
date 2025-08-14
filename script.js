07ae9f1125fee1e4c4441a4a19fa241d164ac630
eduarda608 <eduarda.andrade05@escola.pr.gov.br>
2025-08-14 14:56:42 -0300

    Update script.js3

--- script.js
+++ script.js
@@ -6,29 +6,35 @@ let jogador1 = {
   x: 100,
   y: 100,
   cor: '#ff0000',
-  velocidade: 5
+  velocidade: 5,
+  largura: 50,
+  altura: 50
 };
 
 let jogador2 = {
-  x: 700,
+  x: 650,
   y: 100,
   cor: '#0000ff',
-  velocidade: 5
+  velocidade: 5,
+  largura: 50,
+  altura: 50
 };
 
 let bandeira1 = {
   x: 100,
   y: 500,
-  cor: '#ff0000'
+  cor: '#ff0000',
+  largura: 20,
+  altura: 20
 };
 
 let bandeira2 = {
-  x: 700,
+  x: 680,
   y: 500,
-  cor: '#0000ff'
+  cor: '#0000ff',
+  largura: 20,
+  altura: 20
 };
-
-let projeteis = [];
 
 document.addEventListener('keydown', (e) => {
   if (e.key === 'w') {
@@ -62,15 +68,6 @@ document.addEventListener('keydown', (e) => {
   if (e.key === 'ArrowRight') {
     jogador2.x += jogador2.velocidade;
   }
-  
-  if (e.key === ' ') {
-    projeteis.push({
-      x: jogador1.x,
-      y: jogador1.y,
-      cor: jogador1.cor,
-      velocidade: 10
-    });
-  }
 });
 
 function desenhar() {
@@ -79,22 +76,16 @@ function desenhar() {
   ctx.fillRect(0, 0, tela.width, tela.height);
   
   ctx.fillStyle = jogador1.cor;
-  ctx.fillRect(jogador1.x, jogador1.y, 50, 50);
+  ctx.fillRect(jogador1.x, jogador1.y, jogador1.largura, jogador1.altura);
   
   ctx.fillStyle = jogador2.cor;
-  ctx.fillRect(jogador2.x, jogador2.y, 50, 50);
+  ctx.fillRect(jogador2.x, jogador2.y, jogador2.largura, jogador2.altura);
   
   ctx.fillStyle = bandeira1.cor;
-  ctx.fillRect(bandeira1.x, bandeira1.y, 20, 20);
+  ctx.fillRect(bandeira1.x, bandeira1.y, bandeira1.largura, bandeira1.altura);
   
   ctx.fillStyle = bandeira2.cor;
-  ctx.fillRect(bandeira2.x, bandeira2.y, 20, 20);
-  
-  for (let i = 0; i < projeteis.length; i++) {
-    ctx.fillStyle = projeteis[i].cor;
-    ctx.fillRect(projeteis[i].x, projeteis[i].y, 10, 10);
-    projeteis[i].x += projeteis[i].velocidade;
-  }
+  ctx.fillRect(bandeira2.x, bandeira2.y, bandeira2.largura, bandeira2.altura);
 }
 
 setInterval(() => {
